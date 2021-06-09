4a1790bbb813daa1f2e1fc4dcbf866a894b5110b
Miguel Sousa <miguelsousa.ams@gmail.com>
2021-06-09 21:39:00 +0100

    add text comparision

--- src/sections/TextCompare.js
+++ src/sections/TextCompare.js
@@ -7,70 +7,91 @@ function TextCompare() {
 
     const [value, setValue] = useState('');
     const { voices, speak } = useSpeechSynthesis();
-  
+
     const changeSelectedVoice = e => setSpeechVoice(e.target.selectedIndex);
 
     const [sttValue, setSttValue] = useState('');
     const { listen, listening, stop } = useSpeechRecognition({
         onResult: (result) => {
-        setSttValue(result);
+            setSttValue(result);
         },
     });
 
+    const [compareResult, setCompareResult] = useState([]);
+
+    const checkCenas = (text, speach) => {
+        var a = text.toLowerCase().split("");
+        var d = speach.toLowerCase().split("");
+
+        var biggerSize = a.length > d.length ? a.length : d.length;
+
+        var result = Array.from({ length: biggerSize }, (v, k) => k).map((letter, k) => {
+            return {
+                letter: d[k],
+                isEqual: a[k] === d[k]
+            }
+        });
+
+        setCompareResult(result);
+    }
+
     const callListen = () => {
-        if(listening){
-          stop();
-        } 
+        if (listening) {
+            stop();
+            (sttValue && value) && checkCenas(value, sttValue);
+        }
         listen();
     }
 
-
-
-    return(
+    return (
         <>
-        <div>
             <div>
-                <label className="label">Select your language</label>
-                <select name="voices" id="voices" onChange={""} >
-                {
-                    voices.map((voice,key) => {
-                    return (voice.default ? (
-                        <option 
-                        key={key} 
-                        value={voice.lang}
-                        selected
-                        >{voice.name}</option>
-                    ) : (
-                        <option 
-                        key={key} 
-                        value={voice.lang}
-                        >{voice.name}</option>
-                    ));
-                    })
-                }
-                </select>
-            </div> 
-
-            <div>
-                <label className="label">Write here your text</label>
-                <textarea
-                    value={value}
-                    onChange={(event) => setValue(event.target.value)}
-                />
-            </div>
-            
-            <div id="textCompareRecordContainer">
-                <div id="textCompareRecordLabel">
-                    <label className="label">Record your audio</label>   
-                    <button onClick={callListen.bind()}>
-                        {!listening ? ("Record") : ("Stop")}
-                    </button>
+                <div>
+                    <label className="label">Select your language</label>
+                    <select name="voices" id="voices" onChange={""} >
+                        {
+                            voices.map((voice, key) => {
+                                return (voice.default ? (
+                                    <option
+                                        key={key}
+                                        value={voice.lang}
+                                        selected
+                                    >{voice.name}</option>
+                                ) : (
+                                    <option
+                                        key={key}
+                                        value={voice.lang}
+                                    >{voice.name}</option>
+                                ));
+                            })
+                        }
+                    </select>
                 </div>
-                <label className="label" id="strongLabel">Your text to speech will appear here:</label>
-                <p>{sttValue}</p>
-                {/*listening && <div>Go ahead I'm listening</div>*/}
+
+                <div>
+                    <label className="label">Write here your text</label>
+                    <textarea
+                        value={value}
+                        onChange={(event) => setValue(event.target.value)}
+                    />
+                </div>
+
+                <div id="textCompareRecordContainer">
+                    <div id="textCompareRecordLabel">
+                        <label className="label">Record your audio</label>
+                        <button onClick={callListen.bind()}>
+                            {!listening ? ("Record") : ("Stop")}
+                        </button>
+                    </div>
+                    <label className="label" id="strongLabel">Your text to speech will appear here:</label>
+                    <p>{sttValue}</p>
+                    {/*listening && <div>Go ahead I'm listening</div>*/}
+                    {compareResult.length > 0 && compareResult.map(letter => {
+                        const classLetter = letter.isEqual ? "green" : "red";
+                        return (<span className={classLetter}>{letter.letter ? letter.letter : " "}</span>)
+                    })}
+                </div>
             </div>
-        </div>
         </>
     );
 }
